ec574bbf5d959a8ac37b9cf85398364735fd46df
Sarim Jabbar <sarimjabbar786@gmail.com>
2024-09-13 01:15:03 +0500

    Add files via upload
    Bulb ON/OFF

---
+++ Java/index.js
@@ -0,0 +1,12 @@
+var activeBulb = "https://cdn.mos.cms.futurecdn.net/HaPnm6P7TZhPQGGUxtDjAg-1200-80.jpg";
+
+
+function handleBulb(ahmed){
+    if (ahmed.src == activeBulb) {
+        
+ahmed.src = 
+    "https://m.media-amazon.com/images/I/61rCDYkxBbL.jpg"
+
+
+    } else ahmed.src = activeBulb;
+    }
